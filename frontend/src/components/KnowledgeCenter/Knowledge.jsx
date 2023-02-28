1fd80ea74c646c167bc3631da2584981ca945096
Prashanth g <112794658+Prashanthg01@users.noreply.github.com>
2023-02-28 15:10:35 +0530

    Commit message

--- frontend/src/components/KnowledgeCenter/Knowledge.jsx
+++ frontend/src/components/KnowledgeCenter/Knowledge.jsx
@@ -1,60 +1,43 @@
 import { useSnackbar } from 'notistack';
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { addToWishlist, removeFromWishlist } from '../../actions/wishlistAction';
+import {
+  addToWishlist,
+  removeFromWishlist,
+} from '../../actions/wishlistAction';
 
+const Knowledge = ({ _id, name, images, description }) => {
+  const dispatch = useDispatch();
+  const { enqueueSnackbar } = useSnackbar();
 
-const Knowledge = ({_id,name,images,description}) => {
-    const dispatch = useDispatch();
-    const { enqueueSnackbar } = useSnackbar();
-  
-    const { wishlistItems } = useSelector((state) => state.wishlist);
-  
-    const itemInWishlist = wishlistItems.some((i) => i.product === _id);
-  
-    const addToWishlistHandler = () => {
-      if (itemInWishlist) {
-        dispatch(removeFromWishlist(_id));
-        enqueueSnackbar('Remove From Wishlist', { variant: 'success' });
-      } else {
-        dispatch(addToWishlist(_id));
-        enqueueSnackbar('Added To Wishlist', { variant: 'success' });
-      }
-    };
+  const { wishlistItems } = useSelector((state) => state.wishlist);
 
- 
+  const itemInWishlist = wishlistItems.some((i) => i.product === _id);
+
+  const addToWishlistHandler = () => {
+    if (itemInWishlist) {
+      dispatch(removeFromWishlist(_id));
+      enqueueSnackbar('Remove From Wishlist', { variant: 'success' });
+    } else {
+      dispatch(addToWishlist(_id));
+      enqueueSnackbar('Added To Wishlist', { variant: 'success' });
+    }
+  };
+
   return (
-    
     <div className="cards">
-    {/* <!-- image & product title --> */}
-    <div
-       
-        className="flex flex-col items-center text-center group "
-      >
+      {/* <!-- image & product title --> */}
+      <div className="flex flex-col items-center text-center group cardin">
         <div>
-          <img
-            draggable="false"
-            src={images && images[0].url}
-            alt=""
-          />
+          <img draggable="false" src={images && images[0].url} alt="" />
         </div>
-        <h2 className="text-sm group-hover:text-primary-blue text-center">
-          {name.length > 85 ? `${name.substring(0, 85)}...` : name}
-        </h2>
+        <h2>{name.length > 85 ? `${name.substring(0, 85)}...` : name}</h2>
         <p>{description}</p>
       </div>
-   {/* <!-- image & product title --> */}
-    {/* <!-- product description --> */}
-    <div className="flex flex-col gap-2 items-start">
-     
-
-     
+      <div className="flex flex-col gap-2 items-start"></div>
     </div>
- 
-  </div>
+  );
+};
 
-  )
-}
-
-export default Knowledge
+export default Knowledge;
